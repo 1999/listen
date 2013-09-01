aa621dc7ca46673cfc81dfb7edd7c861018306cc
Dmitry Sorin <info@staypositive.ru>
2013-09-01 23:16:22 +0400

    VK interface

---
+++ src/modules/vk.js
@@ -0,0 +1,21 @@
+VK = (function () {
+    "ust strict";
+
+    return createModule("VK", {
+        searchMusic: function VK_searchMusic() {
+
+        },
+
+        getCurrent: function VK_getCurrent() {
+
+        },
+
+        repost: function VK_repost() {
+
+        },
+
+        getProfileData: function VK_getProfileData() {
+
+        }
+    });
+})();
