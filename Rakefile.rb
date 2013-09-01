ec9383c4fbb92cd4f7930758dbbe2853814300a5
Dmitry Sorin <info@staypositive.ru>
2013-09-01 20:18:20 +0400

    Templates ready

--- Rakefile.rb
+++ Rakefile.rb
@@ -13,67 +13,73 @@ SRC_PATH = "src"
 
 desc "Writes i18n data to messages.json files"
 task :i18n do
-	puts "Write i18n data..."
+    puts "Write i18n data..."
 
-	localesDir = File.join(SRC_PATH, "_locales")
-	i18nKeys = Array.new
-	localeData = Hash.new
+    localesDir = File.join(SRC_PATH, "_locales")
+    i18nKeys = Array.new
+    localeData = Hash.new
 
-	# delete old built files
-	FileUtils.remove_dir(localesDir, true)
+    # delete old built files
+    FileUtils.remove_dir(localesDir, true)
 
-	# create new data
-	JSON.parse(File.open("i18n.json").read).each do |i18nKey, value|
-		i18nKeys.push(i18nKey)
+    # create new data
+    JSON.parse(File.open("i18n.json").read).each do |i18nKey, value|
+        i18nKeys.push(i18nKey)
 
-		value.each do |locale, data|
-			localeData[locale] = localeData[locale] || Hash.new
-			localeData[locale][i18nKey] = {"message" => data}
-		end
-	end
+        value.each do |locale, data|
+            localeData[locale] = localeData[locale] || Hash.new
+            localeData[locale][i18nKey] = {"message" => data}
+        end
+    end
 
-	# write files
-	localeData.each do |locale, data|
-		localeDir = File.join(localesDir, locale)
-		FileUtils.mkdir_p(localeDir)
+    # write files
+    localeData.each do |locale, data|
+        localeDir = File.join(localesDir, locale)
+        FileUtils.mkdir_p(localeDir)
 
-		File.open(File.join(localeDir, "messages.json"), "w") do |f|
-			f.write(JSON.generate(data))
-		end
-	end
+        File.open(File.join(localeDir, "messages.json"), "w") do |f|
+            f.write(JSON.generate(data))
+        end
+    end
 end
 
 desc "Rebuild config from settings and other data"
 task :rebuildConfig do
-	puts "Rebuilding config file..."
+    puts "Rebuilding config file..."
 
-	File.open(File.join(SRC_PATH, "config.js"), "w") do |f|
-		configChunks = {
-			"default_settings_local" => JSON.parse(File.open("settings.json").read).local,
-			"default_settings_sync" => JSON.parse(File.open("settings.json").read).sync
-		}
+    File.open(File.join(SRC_PATH, "config.js"), "w") do |f|
+        configChunks = {
+            "default_settings_local" => JSON.parse(File.open("settings.json").read).local,
+            "default_settings_sync" => JSON.parse(File.open("settings.json").read).sync
+        }
 
-		f.write("Config = " + JSON.generate(configChunks))
-	end
+        f.write("Config = " + JSON.generate(configChunks))
+    end
 end
 
 desc "Builds templates into one file"
 task :templates do
-	puts "Combinind templates into one file..."
+    puts "Combinind templates into one file..."
 
-	combined = Hash.new
-	Dir.glob(File.join("templates", "*.mustache")).each do |fileName|
-		combined[File.basename(fileName, ".mustache")] = File.open(fileName).read
-	end
+    combined = Hash.new
+    Dir.glob(File.join("templates", "*.mustache")).each do |fileName|
+        fileContents = []
 
-	File.open(File.join(SRC_PATH, "sandbox", "alltemplates.js"), "w") do |f|
-		f.write("Templates = " + JSON.pretty_generate(combined, {:indent => "    "}))
-	end
+        IO.readlines(fileName).each do |line|
+            fileContents.push(line.strip)
+        end
+
+        combined[File.basename(fileName, ".mustache")] = fileContents.join
+    end
+
+    File.open(File.join(SRC_PATH, "sandbox", "alltemplates.js"), "w") do |f|
+        f.write("Templates = " + JSON.pretty_generate(combined, {:indent => "    "}))
+    end
 end
 
 desc "Run this after you have cloned the repo"
 task :default do
-	Rake::Task["i18n"].execute
-	Rake::Task["templates"].execute
-	Rake::Task["rebuildConfig"].execute
+    Rake::Task["i18n"].execute
+    Rake::Task["templates"].execute
+    Rake::Task["rebuildConfig"].execute
 end
