2607477d6c42db26dac78255a3cfafe342a7f90f
Dmitry Sorin <info@staypositive.ru>
2013-10-27 17:20:58 +0400

    Show call-to-action notification to all updating users, stop current test

--- src/background/core.js
+++ src/background/core.js
@@ -42,7 +42,7 @@ window.onerror = function(msg, url, line) {
     chrome.notifications && chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
         switch (notificationId) {
             case "update2to3":
-            case "update3to31":
+            case "update":
                 chrome.notifications.clear(notificationId, function () {});
 
                 if (buttonIndex === 0) {
@@ -114,44 +114,44 @@ window.onerror = function(msg, url, line) {
                     }, function () {});
                 }
 
-                // run vkPeopleUsePlaylists test
                 // show "call-to-action" notification
-                if (currentVersion === "3.1") {
-                    chrome.storage.local.set({"settings.tests": ["vkPeopleUsePlaylists"]});
+                chrome.storage.local.get({
+                    "settings.songsPlayed": Config.default_settings_local.songsPlayed,
+                    "settings.vkToken": Config.default_settings_local.vkToken
+                }, function (records) {
+                    var needNotify = false;
+                    var notificationBody;
 
-                    chrome.storage.local.get({
-                        "settings.songsPlayed": Config.default_settings_local.songsPlayed,
-                        "settings.vkToken": Config.default_settings_local.vkToken
-                    }, function (records) {
-                        var needNotify = false;
-                        var notificationBody;
+                    if (!records["settings.vkToken"].length) { // show notification to guests
+                        needNotify = true;
+                        notificationBody = chrome.i18n.getMessage("notificationUpdateCallToActionGuests", appName);
+                    } else if (records["settings.songsPlayed"] < 20) { // show notification to users who don't use the app often
+                        needNotify = true;
+                        notificationBody = chrome.i18n.getMessage("notificationUpdateCallToActionUsers", appName);
+                    }
 
-                        if (!records["settings.vkToken"].length) { // show notification to guests
-                            needNotify = true;
-                            notificationBody = chrome.i18n.getMessage("notificationUpdate3to31CallToActionGuests", appName);
-                        } else if (records["settings.songsPlayed"] < 20) { // show notification to users who don't use the app often
-                            needNotify = true;
-                            notificationBody = chrome.i18n.getMessage("notificationUpdate3to31CallToActionUsers", appName);
-                        }
+                    if (needNotify && chrome.notifications) {
+                        chrome.notifications.create("update", {
+                            type: "basic",
+                            iconUrl: chrome.runtime.getURL("pics/icons/128.png"),
+                            title: chrome.i18n.getMessage("notificationUpdateTitle", appName),
+                            message: notificationBody,
+                            buttons: [
+                                {
+                                    title: chrome.i18n.getMessage("yesGogogo")
+                                },
+                                {
+                                    title: chrome.i18n.getMessage("no")
+                                }
+                            ]
+                        }, function () {});
+                    }
+                });
 
-                        if (needNotify && chrome.notifications) {
-                            chrome.notifications.create("update3to31", {
-                                type: "basic",
-                                iconUrl: chrome.runtime.getURL("pics/icons/128.png"),
-                                title: chrome.i18n.getMessage("notificationUpdateTitle", appName),
-                                message: notificationBody,
-                                buttons: [
-                                    {
-                                        title: chrome.i18n.getMessage("yesGogogo")
-                                    },
-                                    {
-                                        title: chrome.i18n.getMessage("no")
-                                    }
-                                ]
-                            }, function () {});
-                        }
-                    });
-                }
+                // run vkPeopleUsePlaylists test
+                // if (currentVersion === "3.1") {
+                //     chrome.storage.local.set({"settings.tests": ["vkPeopleUsePlaylists"]});
+                // }
 
                 chrome.storage.local.get("installId", function (records) {
                     CPA.sendEvent("Lyfecycle", "Update", {
